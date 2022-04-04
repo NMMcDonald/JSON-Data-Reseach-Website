3e12029caefd432ccffd4ffb4eef237eaa71fcf1
NMMcDonald <nicolasmmcdonald@gmail.com>
2022-04-04 09:56:56 -0700

    4/4

--- webapp.py
+++ webapp.py
@@ -1,1 +1,19 @@
+from flask import Flask, url_for, render_template, request
 
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/Time-To-beat-Compared-to-Metacritic-Rating")
+def render_second():
+    return render_template('page1.html')
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
